3f8b22861c6297c0eb52739c1023b1bb8f9c6ed6
anastasia2099 <an.pinchuk99@gmail.com>
2022-09-19 21:16:44 +0300

    This is a new commit for what I originally planned to be amended

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+"use script";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+//интерполяция
+const category = 'toys';
+console.log(`https://someurl.com/${category}`/4);
+
+const user = 'Ivan';
+alert(`Hello, ${user}`);
